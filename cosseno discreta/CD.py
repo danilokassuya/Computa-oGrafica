920127ccdeec15b55fa3d675b5638979d8366c41
danilo <danilo.kassuya@hotmail.com>
2022-09-26 21:25:43 -0300

    Discreta finalizado

--- cosseno discreta/CD.py
+++ cosseno discreta/CD.py
@@ -1,3 +1,4 @@
+from multiprocessing.resource_sharer import stop
 from socket import SOMAXCONN
 import statistics
 from tokenize import Double
@@ -5,52 +6,62 @@ from PIL import Image
 import numpy as np
 import math
 import cv2
-#define alfa(x,a1,a2) ((x==0) ? (a1) : (a2))
-im = Image.open('teste5.jpg','r').convert('L')
-im = cv2.imread
-im.save("greyscale.jpg")
-rgb = list(im.getdata())
 
-#pixel = im.load()
-pixel = ((16,  11,  10,  16,  24,  40,  51,  61),
-  (12,  12,  14,  19,  26,  58,  60,  55),
-  (14,  13,  16,  24,  40,  57,  69,  56),
-  (14,  17,  22,  29,  51,  87,  80,  62),
-  (18,  22,  37,  56,  68, 109, 103,  77),
-  (24,  35,  55,  64,  81, 104, 113,  92),
-  (49,  64,  78,  87, 103, 121, 120, 101),
-  (72,  92,  95,  98, 112, 100, 103,  99))
+pixel = cv2.imread('teste5.jpg',1)
+pixel = cv2.cvtColor(pixel, cv2.COLOR_BGR2GRAY)
 soma = 0
-#largura, altura = im.size
-largura = 8
-altura = 8
-print(largura,altura)
-n = altura
-i = 0 
-u = 0
-v = 0
+altura = pixel.shape[0]
+largura = pixel.shape[1]
 soma = 0
-saida = Image.new('L', (largura,altura),color="black")
-media = saida.load()
-for u in range(largura):
-    for v in range(altura):
-        for i in range(largura):
-            for j in range(altura):
-                bidi = ((2*i) + 1)*math.pi*u/(2*largura)
-                bidi2 = ((2*j) + 1)*math.pi*v/(2*altura)
+
+media = np.zeros((altura,largura), np.uint8)
+array = [[0 for x in range(largura)] for y in range(altura)] 
+for u in range(altura):
+    for v in range(largura):
+        for i in range(altura):
+            for j in range(largura):
+                bidi = ((2*i) + 1)*math.pi*u/(2*altura)
+                bidi2 = ((2*j) + 1)*math.pi*v/(2*largura)
                 soma += ((pixel[i][j]) * math.cos(bidi) * math.cos(bidi2))
         if u == 0:
-            alfa  = 1.0/math.sqrt(largura)
-        else:
-            alfa = math.sqrt(2.0/largura)
-        soma *= alfa
-        if v == 0:
             alfa  = 1.0/math.sqrt(altura)
         else:
             alfa = math.sqrt(2.0/altura)
         soma *= alfa
-        print(int(soma))
-        media[u,v] = int(soma)
-        print(media[u,v])
+        if v == 0:
+            alfa  = 1.0/math.sqrt(largura)
+        else:
+            alfa = math.sqrt(2.0/largura)
+        soma *= alfa
+        array[u][v] = soma
+        if soma < 0: 
+            soma = 0
+        if soma > 255:
+            soma = 1
+        media[u][v] = soma
         soma = 0
-saida.save('cosseno.tif')
+cv2.imwrite("cosseno.jpg",media)
+
+soma = 0
+i = 0 
+u = 0
+v = 0
+media = np.zeros((altura,largura,3), np.uint8)
+for i in range(altura):
+    for j in range(largura):
+        for u in range(altura):
+            for v in range(largura):
+                bidi = float(((2*i) + 1)*math.pi*u/(2*altura))
+                bidi2 = float(((2*j) + 1)*math.pi*v/(2*largura))
+                if u == 0:
+                    alfaA  = 1.0/math.sqrt(altura)
+                else:
+                    alfaA = math.sqrt(2.0/altura)
+                if v == 0:
+                    alfaB = 1.0/math.sqrt(largura)
+                else:
+                    alfaB = math.sqrt(2.0/largura)
+                soma += (array[u][v] * math.cos(bidi) * math.cos(bidi2) * alfaA * alfaB)
+        media[i][j]= (int(soma))
+        soma = 0
+cv2.imwrite("invertida.jpg",media)
--- cosseno discreta/CDI.py
+++ cosseno discreta/CDI.py
@@ -4,20 +4,14 @@ from tokenize import Double
 from PIL import Image
 import numpy as np
 import math
-im = Image.open('cosseno.jpg','r')
-
-#pixel = im.load()
-pixel = ([ 461, -168 , -14 ,  30 , -31 ,   8 ,   1,   -2],
- [-194  , -1 ,  38 ,   4 ,   6  ,  3  ,  5 ,  -5],
- [  32  , 43,   10 , -22  , 15  ,-10  , -5 ,   4],
- [  -2 , -27  , -1   , 0  , -2  ,  7  ,  4  , -3],
- [   2 ,  11 ,   0  ,  1 ,   4  , -4 ,   1  , -2],
- [   0  , -3  ,  2  ,  2  ,  0  , -2  ,  0  ,  2],
- [  -9  ,  6  ,  4 ,  -8  ,  7 ,  -1  , -6   , 7],
- [   8  , -1 ,  -3 ,   1 ,   2  ,  3  , -1 ,  -1])
+import cv2
+pixel = cv2.imread('cosseno.jpg',1)
+pixel = cv2.cvtColor(pixel, cv2.COLOR_BGR2GRAY)
 soma = 0
-largura, altura = im.size
+altura = pixel.shape[0]
+largura = pixel.shape[1]
 print(largura,altura)
+print(pixel)
 n = altura
 i = 0 
 u = 0
@@ -25,24 +19,23 @@ v = 0
 alfa1 = 1.0/math.sqrt(n)
 alfa2 = math.sqrt(2.0/n)
 soma = 0
-saida = Image.new('L', (largura,altura),color="black")
-media = saida.load()
-for i in range(largura):
-    for j in range(altura):
-        for u in range(largura):
-            for v in range(altura):
-                bidi = float(((2*i) + 1)*math.pi*u/(2*largura))
-                bidi2 = float(((2*j) + 1)*math.pi*v/(2*altura))
+media = np.zeros((altura,largura,3), np.uint8)
+for i in range(altura):
+    for j in range(largura):
+        for u in range(altura):
+            for v in range(largura):
+                bidi = float(((2*i) + 1)*math.pi*u/(2*altura))
+                bidi2 = float(((2*j) + 1)*math.pi*v/(2*largura))
                 if u == 0:
-                    alfaA  = 1.0/math.sqrt(largura)
+                    alfaA  = 1.0/math.sqrt(altura)
                 else:
-                    alfaA = math.sqrt(2.0/largura)
+                    alfaA = math.sqrt(2.0/altura)
                 if v == 0:
-                    alfaB = 1.0/math.sqrt(altura)
+                    alfaB = 1.0/math.sqrt(largura)
                 else:
-                    alfaB = math.sqrt(2.0/altura)
+                    alfaB = math.sqrt(2.0/largura)
                 soma += (pixel[u][v] * math.cos(bidi) * math.cos(bidi2) * alfaA * alfaB)
-        print(int(soma))
-        media[i,j] = (int(soma))
+        print(soma)
+        media[j][i]= (int(soma))
         soma = 0
-saida.save('invertida.jpg')
+cv2.imwrite("invertida.jpg",media)
